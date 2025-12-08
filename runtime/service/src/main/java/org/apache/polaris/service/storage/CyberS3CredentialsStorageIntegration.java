a0dfcae244d3a4492778afaa5dcbb08e741c4e03
cccs-cat001 <reginald.arsenault@cyber.gc.ca>
2025-12-08 11:13:58 -0400

    added custom storage integration preliminary run

---
+++ runtime/service/src/main/java/org/apache/polaris/service/storage/CyberS3CredentialsStorageIntegration.java
@@ -0,0 +1,339 @@
+package org.apache.polaris.service.storage;
+
+import static org.apache.polaris.core.config.FeatureConfiguration.STORAGE_CREDENTIAL_DURATION_SECONDS;
+
+import jakarta.annotation.Nonnull;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
+import org.apache.polaris.core.config.RealmConfig;
+import org.apache.polaris.core.storage.InMemoryStorageIntegration;
+import org.apache.polaris.core.storage.StorageAccessConfig;
+import org.apache.polaris.core.storage.StorageAccessProperty;
+import org.apache.polaris.core.storage.StorageUtil;
+import org.apache.polaris.core.storage.aws.AwsStorageConfigurationInfo;
+import org.apache.polaris.core.storage.aws.StsClientProvider;
+import org.apache.polaris.core.storage.aws.StsClientProvider.StsDestination;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.policybuilder.iam.IamConditionOperator;
+import software.amazon.awssdk.policybuilder.iam.IamEffect;
+import software.amazon.awssdk.policybuilder.iam.IamPolicy;
+import software.amazon.awssdk.policybuilder.iam.IamResource;
+import software.amazon.awssdk.policybuilder.iam.IamStatement;
+import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
+import software.amazon.awssdk.services.sts.model.AssumeRoleResponse;
+
+public class CyberS3CredentialsStorageIntegration
+    extends InMemoryStorageIntegration<AwsStorageConfigurationInfo> {
+  private final StsClientProvider stsClientProvider;
+  private final Optional<AwsCredentialsProvider> credentialsProvider;
+
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(CyberS3CredentialsStorageIntegration.class);
+
+  public CyberS3CredentialsStorageIntegration(
+      AwsStorageConfigurationInfo config, StsClient fixedClient) {
+    this(config, (destination) -> fixedClient);
+  }
+
+  public CyberS3CredentialsStorageIntegration(
+      AwsStorageConfigurationInfo config, StsClientProvider stsClientProvider) {
+    this(config, stsClientProvider, Optional.empty());
+  }
+
+  public CyberS3CredentialsStorageIntegration(
+      AwsStorageConfigurationInfo config,
+      StsClientProvider stsClientProvider,
+      Optional<AwsCredentialsProvider> credentialsProvider) {
+    super(config, CyberS3CredentialsStorageIntegration.class.getName());
+    this.stsClientProvider = stsClientProvider;
+    this.credentialsProvider = credentialsProvider;
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public StorageAccessConfig getSubscopedCreds(
+      @Nonnull RealmConfig realmConfig,
+      boolean allowListOperation,
+      @Nonnull Set<String> allowedReadLocations,
+      @Nonnull Set<String> allowedWriteLocations,
+      Optional<String> refreshCredentialsEndpoint) {
+    int storageCredentialDurationSeconds =
+        realmConfig.getConfig(STORAGE_CREDENTIAL_DURATION_SECONDS);
+    AwsStorageConfigurationInfo storageConfig = config();
+    String region = storageConfig.getRegion();
+    String accountId = storageConfig.getAwsAccountId();
+    StorageAccessConfig.Builder accessConfig = StorageAccessConfig.builder();
+
+    if (shouldUseSts(storageConfig)) {
+      AssumeRoleRequest.Builder request =
+          AssumeRoleRequest.builder()
+              .externalId(storageConfig.getExternalId())
+              .roleArn(storageConfig.getRoleARN())
+              .roleSessionName("PolarisCyberS3CredentialsStorageIntegration")
+              .policy(
+                  policyString(
+                          storageConfig,
+                          allowListOperation,
+                          allowedReadLocations,
+                          allowedWriteLocations,
+                          region,
+                          accountId)
+                      .toJson())
+              .durationSeconds(storageCredentialDurationSeconds);
+      credentialsProvider.ifPresent(
+          cp -> request.overrideConfiguration(b -> b.credentialsProvider(cp)));
+
+      @SuppressWarnings("resource")
+      // Note: stsClientProvider returns "thin" clients that do not need closing
+      StsClient stsClient =
+          stsClientProvider.stsClient(StsDestination.of(storageConfig.getStsEndpointUri(), region));
+
+      AssumeRoleResponse response = stsClient.assumeRole(request.build());
+      accessConfig.put(StorageAccessProperty.AWS_KEY_ID, response.credentials().accessKeyId());
+      accessConfig.put(
+          StorageAccessProperty.AWS_SECRET_KEY, response.credentials().secretAccessKey());
+      accessConfig.put(StorageAccessProperty.AWS_TOKEN, response.credentials().sessionToken());
+      Optional.ofNullable(response.credentials().expiration())
+          .ifPresent(
+              i -> {
+                accessConfig.put(
+                    StorageAccessProperty.EXPIRATION_TIME, String.valueOf(i.toEpochMilli()));
+                accessConfig.put(
+                    StorageAccessProperty.AWS_SESSION_TOKEN_EXPIRES_AT_MS,
+                    String.valueOf(i.toEpochMilli()));
+              });
+    }
+
+    if (region != null) {
+      accessConfig.put(StorageAccessProperty.CLIENT_REGION, region);
+    }
+
+    refreshCredentialsEndpoint.ifPresent(
+        endpoint -> {
+          accessConfig.put(StorageAccessProperty.AWS_REFRESH_CREDENTIALS_ENDPOINT, endpoint);
+        });
+
+    URI endpointUri = storageConfig.getEndpointUri();
+    if (endpointUri != null) {
+      accessConfig.put(StorageAccessProperty.AWS_ENDPOINT, endpointUri.toString());
+    }
+    URI internalEndpointUri = storageConfig.getInternalEndpointUri();
+    if (internalEndpointUri != null) {
+      accessConfig.putInternalProperty(
+          StorageAccessProperty.AWS_ENDPOINT.getPropertyName(), internalEndpointUri.toString());
+    }
+
+    if (Boolean.TRUE.equals(storageConfig.getPathStyleAccess())) {
+      accessConfig.put(StorageAccessProperty.AWS_PATH_STYLE_ACCESS, Boolean.TRUE.toString());
+    }
+
+    if ("aws-us-gov".equals(storageConfig.getAwsPartition()) && region == null) {
+      throw new IllegalArgumentException(
+          String.format(
+              "AWS region must be set when using partition %s", storageConfig.getAwsPartition()));
+    }
+
+    return accessConfig.build();
+  }
+
+  private boolean shouldUseSts(AwsStorageConfigurationInfo storageConfig) {
+    return !Boolean.TRUE.equals(storageConfig.getStsUnavailable());
+  }
+
+  /**
+   * generate an IamPolicy from the input readLocations and writeLocations, optionally with list
+   * support. Credentials will be scoped to exactly the resources provided. If read and write
+   * locations are empty, a non-empty policy will be generated that grants GetObject and optionally
+   * ListBucket privileges with no resources. This prevents us from sending an empty policy to AWS
+   * and just assuming the role with full privileges.
+   */
+  private IamPolicy policyString(
+      AwsStorageConfigurationInfo storageConfigurationInfo,
+      boolean allowList,
+      Set<String> readLocations,
+      Set<String> writeLocations,
+      String region,
+      String accountId) {
+    IamPolicy.Builder policyBuilder = IamPolicy.builder();
+    IamStatement.Builder allowGetObjectStatementBuilder =
+        IamStatement.builder()
+            .effect(IamEffect.ALLOW)
+            .addAction("s3:GetObject")
+            .addAction("s3:GetObjectVersion");
+    Map<String, IamStatement.Builder> bucketListStatementBuilder = new HashMap<>();
+    Map<String, IamStatement.Builder> bucketGetLocationStatementBuilder = new HashMap<>();
+
+    String arnPrefix = arnPrefixForPartition(storageConfigurationInfo.getAwsPartition());
+    String currentKmsKey = storageConfigurationInfo.getCurrentKmsKey();
+    List<String> allowedKmsKeys = storageConfigurationInfo.getAllowedKmsKeys();
+    Stream.concat(readLocations.stream(), writeLocations.stream())
+        .distinct()
+        .forEach(
+            location -> {
+              URI uri = URI.create(location);
+              allowGetObjectStatementBuilder.addResource(
+                  IamResource.create(
+                      arnPrefix + StorageUtil.concatFilePrefixes(parseS3Path(uri), "*", "/")));
+              final var bucket = arnPrefix + StorageUtil.getBucket(uri);
+              if (allowList) {
+                bucketListStatementBuilder
+                    .computeIfAbsent(
+                        bucket,
+                        (String key) ->
+                            IamStatement.builder()
+                                .effect(IamEffect.ALLOW)
+                                .addAction("s3:ListBucket")
+                                .addResource(key))
+                    .addCondition(
+                        IamConditionOperator.STRING_LIKE,
+                        "s3:prefix",
+                        StorageUtil.concatFilePrefixes(trimLeadingSlash(uri.getPath()), "*", "/"));
+              }
+              bucketGetLocationStatementBuilder.computeIfAbsent(
+                  bucket,
+                  key ->
+                      IamStatement.builder()
+                          .effect(IamEffect.ALLOW)
+                          .addAction("s3:GetBucketLocation")
+                          .addResource(key));
+            });
+
+    if (!writeLocations.isEmpty()) {
+      IamStatement.Builder allowPutObjectStatementBuilder =
+          IamStatement.builder()
+              .effect(IamEffect.ALLOW)
+              .addAction("s3:PutObject")
+              .addAction("s3:DeleteObject");
+      writeLocations.forEach(
+          location -> {
+            URI uri = URI.create(location);
+            allowPutObjectStatementBuilder.addResource(
+                IamResource.create(
+                    arnPrefix + StorageUtil.concatFilePrefixes(parseS3Path(uri), "*", "/")));
+          });
+      policyBuilder.addStatement(allowPutObjectStatementBuilder.build());
+      addKmsKeyPolicy(currentKmsKey, allowedKmsKeys, policyBuilder, true, region, accountId);
+    } else {
+      addKmsKeyPolicy(currentKmsKey, allowedKmsKeys, policyBuilder, false, region, accountId);
+    }
+    if (!bucketListStatementBuilder.isEmpty()) {
+      bucketListStatementBuilder
+          .values()
+          .forEach(statementBuilder -> policyBuilder.addStatement(statementBuilder.build()));
+    } else if (allowList) {
+      // add list privilege with 0 resources
+      policyBuilder.addStatement(
+          IamStatement.builder().effect(IamEffect.ALLOW).addAction("s3:ListBucket").build());
+    }
+
+    bucketGetLocationStatementBuilder
+        .values()
+        .forEach(statementBuilder -> policyBuilder.addStatement(statementBuilder.build()));
+    return policyBuilder.addStatement(allowGetObjectStatementBuilder.build()).build();
+  }
+
+  private static void addKmsKeyPolicy(
+      String kmsKeyArn,
+      List<String> allowedKmsKeys,
+      IamPolicy.Builder policyBuilder,
+      boolean canWrite,
+      String region,
+      String accountId) {
+
+    IamStatement.Builder allowKms = buildBaseKmsStatement(canWrite);
+    boolean hasCurrentKey = kmsKeyArn != null;
+    boolean hasAllowedKeys = hasAllowedKmsKeys(allowedKmsKeys);
+
+    if (hasCurrentKey) {
+      addKmsKeyResource(kmsKeyArn, allowKms);
+    }
+
+    if (hasAllowedKeys) {
+      addAllowedKmsKeyResources(allowedKmsKeys, allowKms);
+    }
+
+    // Add KMS statement if we have any KMS key configuration
+    if (hasCurrentKey || hasAllowedKeys) {
+      policyBuilder.addStatement(allowKms.build());
+    } else if (!canWrite) {
+      // Only add wildcard KMS access for read-only operations when no specific keys are configured
+      // this check is for minio because it doesn't have region or account id
+      if (region != null && accountId != null) {
+        addAllKeysResource(region, accountId, allowKms);
+        policyBuilder.addStatement(allowKms.build());
+      }
+    }
+  }
+
+  private static IamStatement.Builder buildBaseKmsStatement(boolean canEncrypt) {
+    IamStatement.Builder allowKms =
+        IamStatement.builder()
+            .effect(IamEffect.ALLOW)
+            .addAction("kms:GenerateDataKeyWithoutPlaintext")
+            .addAction("kms:DescribeKey")
+            .addAction("kms:Decrypt")
+            .addAction("kms:GenerateDataKey");
+
+    if (canEncrypt) {
+      allowKms.addAction("kms:Encrypt");
+    }
+
+    return allowKms;
+  }
+
+  private static void addKmsKeyResource(String kmsKeyArn, IamStatement.Builder allowKms) {
+    if (kmsKeyArn != null) {
+      LOGGER.debug("Adding KMS key policy for key {}", kmsKeyArn);
+      allowKms.addResource(IamResource.create(kmsKeyArn));
+    }
+  }
+
+  private static boolean hasAllowedKmsKeys(List<String> allowedKmsKeys) {
+    return allowedKmsKeys != null && !allowedKmsKeys.isEmpty();
+  }
+
+  private static void addAllowedKmsKeyResources(
+      List<String> allowedKmsKeys, IamStatement.Builder allowKms) {
+    allowedKmsKeys.forEach(
+        keyArn -> {
+          LOGGER.debug("Adding allowed KMS key policy for key {}", keyArn);
+          allowKms.addResource(IamResource.create(keyArn));
+        });
+  }
+
+  private static void addAllKeysResource(
+      String region, String accountId, IamStatement.Builder allowKms) {
+    String allKeysArn = arnKeyAll(region, accountId);
+    allowKms.addResource(IamResource.create(allKeysArn));
+    LOGGER.debug("Adding KMS key policy for all keys in account {}", accountId);
+  }
+
+  private static String arnKeyAll(String region, String accountId) {
+    return String.format("arn:aws:kms:%s:%s:key/*", region, accountId);
+  }
+
+  private static String arnPrefixForPartition(String awsPartition) {
+    return String.format("arn:%s:s3:::", awsPartition != null ? awsPartition : "aws");
+  }
+
+  private static @Nonnull String parseS3Path(URI uri) {
+    String bucket = StorageUtil.getBucket(uri);
+    String path = trimLeadingSlash(uri.getPath());
+    return String.join("/", bucket, path);
+  }
+
+  private static @Nonnull String trimLeadingSlash(String path) {
+    if (path.startsWith("/")) {
+      path = path.substring(1);
+    }
+    return path;
+  }
+}
---
+++ runtime/service/src/main/java/org/apache/polaris/service/storage/CyberStorageIntegrationProviderImpl.java
@@ -0,0 +1,121 @@
+package org.apache.polaris.service.storage;
+
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.auth.http.HttpTransportFactory;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.ServiceOptions;
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import jakarta.annotation.Priority;
+import jakarta.enterprise.inject.Alternative;
+import jakarta.inject.Inject;
+import java.time.Clock;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Supplier;
+import org.apache.polaris.core.config.RealmConfig;
+import org.apache.polaris.core.storage.PolarisStorageActions;
+import org.apache.polaris.core.storage.PolarisStorageConfigurationInfo;
+import org.apache.polaris.core.storage.PolarisStorageIntegration;
+import org.apache.polaris.core.storage.PolarisStorageIntegration.ValidationResult;
+import org.apache.polaris.core.storage.StorageAccessConfig;
+import org.apache.polaris.core.storage.aws.AwsCredentialsStorageIntegration;
+import org.apache.polaris.core.storage.aws.AwsStorageConfigurationInfo;
+import org.apache.polaris.core.storage.aws.StsClientProvider;
+import org.apache.polaris.core.storage.azure.AzureCredentialsStorageIntegration;
+import org.apache.polaris.core.storage.azure.AzureStorageConfigurationInfo;
+import org.apache.polaris.core.storage.gcp.GcpCredentialsStorageIntegration;
+import org.apache.polaris.core.storage.gcp.GcpStorageConfigurationInfo;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+
+@Alternative
+@Priority(10)
+public class CyberStorageIntegrationProviderImpl {
+  private final StsClientProvider stsClientProvider;
+  private final Optional<AwsCredentialsProvider> stsCredentials;
+  private final Supplier<GoogleCredentials> gcpCredsProvider;
+
+  @SuppressWarnings("CdiInjectionPointsInspection")
+  @Inject
+  public CyberStorageIntegrationProviderImpl(
+      StorageConfiguration storageConfiguration, StsClientProvider stsClientProvider, Clock clock) {
+    this(
+        stsClientProvider,
+        Optional.ofNullable(storageConfiguration.stsCredentials()),
+        storageConfiguration.gcpCredentialsSupplier(clock));
+  }
+
+  public CyberStorageIntegrationProviderImpl(
+      StsClientProvider stsClientProvider,
+      Optional<AwsCredentialsProvider> stsCredentials,
+      Supplier<GoogleCredentials> gcpCredsProvider) {
+    this.stsClientProvider = stsClientProvider;
+    this.stsCredentials = stsCredentials;
+    this.gcpCredsProvider = gcpCredsProvider;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public <T extends PolarisStorageConfigurationInfo>
+      @Nullable PolarisStorageIntegration<T> getStorageIntegrationForConfig(
+          PolarisStorageConfigurationInfo polarisStorageConfigurationInfo) {
+    if (polarisStorageConfigurationInfo == null) {
+      return null;
+    }
+    PolarisStorageIntegration<T> storageIntegration;
+    switch (polarisStorageConfigurationInfo.getStorageType()) {
+      case S3:
+        storageIntegration =
+            (PolarisStorageIntegration<T>)
+                new AwsCredentialsStorageIntegration(
+                    (AwsStorageConfigurationInfo) polarisStorageConfigurationInfo,
+                    stsClientProvider,
+                    stsCredentials);
+        break;
+      case GCS:
+        storageIntegration =
+            (PolarisStorageIntegration<T>)
+                new GcpCredentialsStorageIntegration(
+                    (GcpStorageConfigurationInfo) polarisStorageConfigurationInfo,
+                    gcpCredsProvider.get(),
+                    ServiceOptions.getFromServiceLoader(
+                        HttpTransportFactory.class, NetHttpTransport::new));
+        break;
+      case AZURE:
+        storageIntegration =
+            (PolarisStorageIntegration<T>)
+                new AzureCredentialsStorageIntegration(
+                    (AzureStorageConfigurationInfo) polarisStorageConfigurationInfo);
+        break;
+      case FILE:
+        storageIntegration =
+            new PolarisStorageIntegration<>((T) polarisStorageConfigurationInfo, "file") {
+              @Override
+              public StorageAccessConfig getSubscopedCreds(
+                  @Nonnull RealmConfig realmConfig,
+                  boolean allowListOperation,
+                  @Nonnull Set<String> allowedReadLocations,
+                  @Nonnull Set<String> allowedWriteLocations,
+                  Optional<String> refreshCredentialsEndpoint) {
+                return StorageAccessConfig.builder().supportsCredentialVending(false).build();
+              }
+
+              @Override
+              public @Nonnull Map<String, Map<PolarisStorageActions, ValidationResult>>
+                  validateAccessToLocations(
+                      @Nonnull RealmConfig realmConfig,
+                      @Nonnull T storageConfig,
+                      @Nonnull Set<PolarisStorageActions> actions,
+                      @Nonnull Set<String> locations) {
+                return Map.of();
+              }
+            };
+        break;
+      default:
+        throw new IllegalArgumentException(
+            "Unknown storage type " + polarisStorageConfigurationInfo.getStorageType());
+    }
+    return storageIntegration;
+  }
+}
